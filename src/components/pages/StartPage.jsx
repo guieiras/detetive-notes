021884356293c472cacf5cdaa87115406c0c134c
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-11-23 12:04:25 -0200

    Initial Commit

---
+++ src/components/pages/StartPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  Page,
+  Navbar
+} from 'framework7-react';
+
+export default class LoginPage extends Component {
+  render() {
+    return <Page>
+      <Navbar title="Detetive - Ficha de Palpites"></Navbar>
+    </Page>
+  }
+}
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import StartPage from './components/pages/StartPage';
+import NotFoundPage from './components/pages/NotFoundPage';
+
+export default [
+  {
+    path: '/',
+    component: StartPage,
+  },
+  {
+    path: '(.*)',
+    component: NotFoundPage,
+  },
+];
