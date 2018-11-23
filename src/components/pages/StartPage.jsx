e9aff6639324ec8ce80966de03d658d60834412e
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-11-23 12:21:14 -0200

    Added Initial Screen

--- src/components/pages/StartPage.jsx
+++ src/components/pages/StartPage.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import {
+  Block,
+  BlockTitle,
+  List,
+  ListItem,
+  Navbar,
   Page,
-  Navbar
 } from 'framework7-react';
 
 export default class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { games: [] };
+  }
   render() {
     return <Page>
       <Navbar title="Detetive - Ficha de Palpites"></Navbar>
+      <Block strong>
+        <p>Bem-vindo à Ficha de Palpites. Você pode iniciar um novo jogo ou carregar uma configuração existente.</p>
+      </Block>
+      <BlockTitle>Iniciar novo Jogo</BlockTitle>
+      <Block>
+        <List>
+          <ListItem title="Novo Jogo" link="/jogos/novo"></ListItem>
+          <ListItem title="Carregar Jogo" link="/jogos/carregar"></ListItem>
+        </List>
+      </Block>
+      <BlockTitle>Meus Jogos</BlockTitle>
+      <Block>
+        <List>
+          {
+            this.state.games.length === 0 ?
+              <ListItem title="Nenhum jogo encontrado" /> :
+              this.state.games.map((game) => <ListItem title={game.title} />)
+          }
+        </List>
+      </Block>
+
     </Page>
   }
 }
