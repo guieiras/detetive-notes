7210921f4b2bbaddc0afcd845b4c197d1cb181b2
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-11-29 18:20:47 -0200

    Added start page draft management

--- src/components/pages/StartPage.jsx
+++ src/components/pages/StartPage.jsx
@@ -2,22 +2,50 @@ import React, { Component } from 'react';
 import {
   Block,
   BlockTitle,
+  Button,
   List,
   ListItem,
   Navbar,
   Page,
 } from 'framework7-react';
 import currentGame from '../../boundaries/currentGame';
+import db from '../../boundaries/db';
 
 export default class LoginPage extends Component {
   constructor(props) {
     super(props);
     this.onLoad = this.onLoad.bind(this);
-    this.state = { games: [] };
+    this.eraseDraft = this.eraseDraft.bind(this);
+    this.useDraft = this.useDraft.bind(this);
+    this.state = { drafts: [] };
+  }
+
+  componentDidMount() {
+    db.drafts.toArray().then((drafts) => { this.setState({ drafts }); });
   }
 
   onLoad() {
     if (currentGame.isPresent()) {
+      this.$f7router.navigate('/games/current');
+    }
+  }
+
+  eraseDraft(draftId) {
+    return () => {
+      this.$f7.dialog.confirm(
+        'Tem certeza que deseja remover a turma? Não é possível desfazer essa ação.',
+        'Apagar turma',
+        () => {
+          db.drafts.delete(draftId).then(() => {
+            this.componentDidMount();
+          })
+        });
+    }
+  }
+
+  useDraft(draft) {
+    return () => {
+      currentGame.generateFromDraft(draft);
       this.$f7router.navigate('/games/current');
     }
   }
@@ -28,24 +56,20 @@ export default class LoginPage extends Component {
       <Block strong>
         <p>Bem-vindo à Ficha de Palpites. Você pode iniciar um novo jogo ou carregar uma configuração existente.</p>
       </Block>
-      <BlockTitle>Iniciar novo Jogo</BlockTitle>
+      <BlockTitle>Minhas Turmas</BlockTitle>
       <Block>
         <List>
-          <ListItem title="Novo Jogo" link="/games/new"></ListItem>
-          <ListItem title="Carregar Jogo" link="/games/load"></ListItem>
-        </List>
-      </Block>
-      <BlockTitle>Meus Jogos</BlockTitle>
-      <Block>
-        <List>
+          <ListItem title="Criar novo jogo" link="/games/new"></ListItem>
           {
-            this.state.games.length === 0 ?
-              <ListItem title="Nenhum jogo encontrado" /> :
-              this.state.games.map((game) => <ListItem title={game.title} />)
+            this.state.drafts.length === 0 ?
+              <ListItem disabled title="Nenhuma turma encontrada" /> :
+              this.state.drafts.map((draft) => <ListItem key={draft.id} title={draft.name}>
+                <Button fill raised style={{ marginLeft: '5px' }} onClick={this.eraseDraft(draft.id)} color="red" slot="after">Excluir</Button>
+                <Button fill raised style={{ marginLeft: '5px' }} onClick={this.useDraft(draft)} slot="after">Jogar</Button>
+              </ListItem>)
           }
         </List>
       </Block>
-
     </Page>
   }
 }
