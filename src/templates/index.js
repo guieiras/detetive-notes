948a8151ca7a51c3c0392b81c5a7eced5e58a3cd
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-06-25 16:21:41 -0300

    Update templates

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,5 +1,9 @@
-import Classic from './pt-BR/classic.json'
+import ClassicPtBr from './pt-BR/classic.json'
+import CluePtBr from './pt-BR/clue.json'
+import NewPtBr from './pt-BR/new.json'
 
 export default [
-  Classic
+  ClassicPtBr,
+  NewPtBr,
+  CluePtBr,
 ]
